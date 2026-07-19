6b2898e182cd5aa45e0e7768113324bead09a8ef
zapitapps <zapitapps@gmail.com>
2026-07-19 22:16:44 +0100

    Create requestld.js

---
+++ src/middleware/requestld.js
@@ -0,0 +1,8 @@
+const { v4: uuidv4 } = require('uuid');
+
+module.exports = function requestId(req, res, next) {
+  const id = req.headers['x-request-id'] || uuidv4();
+  req.requestId = id;
+  res.setHeader('X-Request-ID', id);
+  next();
+};
